7701c011037b8261ce94a269883971b30941c3a4
527esilvia-sudo <527esilvia@frhsd.com>
2026-02-03 22:01:35 +0000

    prob gonna break smth

---
+++ script.js
@@ -0,0 +1,15 @@
+let userScore = 0
+let computerScore = 0
+let computerChoice 
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    const random = Math.floor(Math.random() choices.length);
+ return choices[random];
+}
+
+computerChoice = getComputerChoice()
+
+document.getElementById
+
+
