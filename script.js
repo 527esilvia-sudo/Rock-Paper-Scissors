a543f8a8e17c7052c075ec05d60adcad6d001552
527esilvia-sudo <527Esilvia@frhsd.com>
2026-02-04 08:46:01 -0500

    now normal

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ let computerScore = 0
 let computerChoice 
 const choices = ["rock", "paper", "scissors"];
 
+const startButton = document.getElementById("playButton");
+const gameSetup = document.querySelector(".game-setup");
+
+
+
+startButton.addEventListener("click", function () {
+  gameSetup.style.display = "block";
+  startButton.style.display = "none";
+});
 function getComputerChoice() {
-    const random = Math.floor(Math.random() choices.length);
+    const random = Math.floor(Math.random() * choices.length);
  return choices[random];
 }
 
 computerChoice = getComputerChoice()
 
-document.getElementById
+document.getElementById('userChoice')
+
 
 
