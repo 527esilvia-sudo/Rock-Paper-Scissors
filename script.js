a1b44beac20bb01b00dce9cf639eb0f243edf792
527esilvia-sudo <527Esilvia@frhsd.com>
2026-02-05 11:55:51 -0500

    not done :(

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let userScore = 0
 let computerScore = 0
-let computerChoice 
+
+let computerChoice
 const choices = ["rock", "paper", "scissors"];
 
 const startButton = document.getElementById("playButton");
@@ -13,8 +14,9 @@ startButton.addEventListener("click", function () {
   startButton.style.display = "none";
 });
 function getComputerChoice() {
-    const random = Math.floor(Math.random() * choices.length);
- return choices[random];
+  const random = Math.floor(Math.random() * choices.length);
+  
+  return choices[random];
 }
 
 computerChoice = getComputerChoice()
@@ -25,6 +27,52 @@ userChoice = document.getElementById('userChoice')
 
 
 
-function playRound(userChoice){
-if userChoice === 'rock' && computerChoice === 'scissors' 
+function playRound(userChoice) {
+
+  if (userChoice === 'rock' && computerChoice === 'scissors') {
+        userWins()
+  }
+   if (userChoice === 'rock' && computerChoice === 'paper') {
+        computerWins()
+  }
+   if (userChoice === 'scissors' && computerChoice === 'rock') {
+        computerWins()
+  }
+   if (userChoice === 'paper' && computerChoice === 'rock') {
+        userWins()
+  }
+   if (userChoice === 'paper' && computerChoice === 'rock') {
+        userWins()
+  }
+   if (userChoice === 'paper' && computerChoice === 'scissors') {
+        computerWins()
+  }
+  if (userChoice === 'scissors' && computerChoice === 'paper') {
+        userWins()
+  }
+    if (userChoice === 'scissors' && computerChoice === 'scissors') {
+        roundTie()
+  }
+    if (userChoice === 'paper' && computerChoice === 'paper') {
+        roundTie()
+  }
+    if (userChoice === 'rock' && computerChoice === 'rock') {
+        roundTie()
+  }
+}
+
+function roundTie() {
+  document.getElementById('roundAnnouncement').innerHTML = `It's a tie!`
+}
+
+function computerWins() {
+  computerScore = computerScore +1
+  document.getElementById('roundAnnouncement').innerHTML = `Computer wins!`
+  document.getElementById('computerScore').innerHTML = `Computer Score: ${computerScore}`
+}
+
+function userWins() {
+  userScore = userScore +1
+  document.getElementById('roundAnnouncement').innerHTML = `${userName} wins!`
+  document.getElementById('userScore').innerHTML = `${userName} Score: ${userScore}`
 }
